156589692ce60cd472eb194703380ed8e9a4a28f
ffernandezz <florcitafer15@gmail.com>
2016-06-21 15:57:25 -0500

    multiplicacion de matrices, primera pregunta

---
+++ multiplication.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h> 
+
+using namespace std;
+
+const int n=1000;
+
+int AA[n][n];
+int BB[n][n];
+int C[n][n];
+void add()
+{
+    int number;
+    srand (time(NULL));
+
+    for(int i=0;i<n;++i)
+        
+        {
+            for(int j=0;j<n;++j)
+            {
+                number=rand()% 30 +1;
+                AA[i][j]=number;
+                BB[i][j]=number+3;
+            }
+         
+        }
+  
+}
+
+void multiplicacion()
+{
+    
+    for(int i=0;i<n;++i)
+        for(int j=0;j<n;++j)
+            C[i][j]=0;
+
+    for(int i=0;i<n;++i)
+        for(int j=0;j<n;++j)
+            for(int k=0;k<n;++k)
+            {
+                C[i][j]+=AA[i][k]*BB[k][j];
+            }
+    
+    cout<<"Finalizo"<<endl;        
+
+}
+
+void print()
+{
+    int count=0;
+    for(int i=0;i<n;++i)
+        for(int j=0;j<n;++j)
+        {
+            cout<<C[i][j]<<endl;
+            ++count;
+
+        }
+
+    cout<<"# elementos: "<<count<<endl;
+            
+
+}
+
+int main (){
+
+    add();
+    multiplicacion();
+    print();
+
+    return 0;
+}
